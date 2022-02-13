e4c8e9c6c7beb975264e9d59d76a5a1bb8c89335
Diane Tritchler <dtritchler92@gmail.com>
2022-02-13 09:33:14 -0600

    front end handlebars

--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,5 @@
+-- Drop Database
 DROP DATABASE IF EXISTS techblog_db;
 
+-- Create Database
 CREATE DATABASE techblog_db;
--- seeds/vote-seeds.js
+++ seeds/vote-seeds.js
@@ -2,23 +2,11 @@ const { Vote } = require('../models');
 
 const votedata = [
     {
-        user_id: 9,
-        blog_id: 3
-    },
-    {
         user_id: 1,
         blog_id: 3
     },
     {
-        user_id: 8,
-        blog_id: 2
-    },
-    {
-        user_id: 8,
-        blog_id: 2
-    },
-    {
-        user_id: 9,
+        user_id: 2,
         blog_id: 3
     },
     {
@@ -27,82 +15,14 @@ const votedata = [
     },
     {
         user_id: 4,
-        blog_id: 3
-    },
-    {
-        user_id: 10,
-        blog_id: 2
-    },
-    {
-        user_id: 3,
-        blog_id: 3
-    },
-    {
-        user_id: 9,
-        blog_id: 2
-    },
-    {
-        user_id: 3,
-        blog_id: 3
-    },
-    {
-        user_id: 10,
-        blog_id: 2
-    },
-    {
-        user_id: 6,
-        blog_id: 1
-    },
-    {
-        user_id: 5,
-        blog_id: 1
-    },
-    {
-        user_id: 6,
-        blog_id: 1
-    },
-    {
-        user_id: 9,
-        blog_id: 8
-    },
-    {
-        user_id: 6,
-        blog_id: 5
-    },
-    {
-        user_id: 6,
-        blog_id: 7
-    },
-    {
-        user_id: 6,
-        blog_id: 1
-    },
-    {
-        user_id: 1,
-        blog_id: 2
-    },
-    {
-        user_id: 10,
-        blog_id: 3
-    },
-    {
-        user_id: 4,
-        blog_id: 1
-    },
-    {
-        user_id: 10,
         blog_id: 2
     },
     {
         user_id: 5,
-        blog_id: 2
+        blog_id: 3
     },
     {
         user_id: 6,
-        blog_id: 2
-    },
-    {
-        user_id: 1,
         blog_id: 2
     },
     {
@@ -110,85 +30,14 @@ const votedata = [
         blog_id: 3
     },
     {
-        user_id: 6,
-        blog_id: 3
-    },
-    {
-        user_id: 6,
-        blog_id: 3
-    },
-    {
-        user_id: 7,
-        blog_id: 1
-    },
-    {
-        user_id: 4,
-        blog_id: 5
-    },
-    {
-        user_id: 2,
-        blog_id: 8
-    },
-    {
-        user_id: 9,
-        blog_id: 0
-    },
-    {
-        user_id: 10,
-        blog_id: 5
-    },
-    {
         user_id: 8,
-        blog_id: 1
-    },
-    {
-        user_id: 10,
-        blog_id: 3
-    },
-    {
-        user_id: 2,
-        blog_id: 3
+        blog_id: 2
     },
     {
         user_id: 9,
-        blog_id: 2
-    },
-    {
-        user_id: 1,
-        blog_id: 1
-    },
-    {
-        user_id: 10,
-        blog_id: 2
-    },
-    {
-        user_id: 10,
         blog_id: 3
-    },
-    {
-        user_id: 5,
-        blog_id: 3
-    },
-    {
-        user_id: 6,
-        blog_id: 2
-    },
-    {
-        user_id: 5,
-        blog_id: 2
-    },
-    {
-        user_id: 6,
-        blog_id: 3
-    },
-    {
-        user_id: 2,
-        blog_id: 3
-    },
-    {
-        user_id: 4,
-        blog_id: 2
     }
+
 ];
 
 const seedVotes = () => Vote.bulkCreate(votedata);
