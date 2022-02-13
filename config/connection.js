177e1c69f6626453274c33e629ede084a511c71c
Diane Tritchler <dtritchler92@gmail.com>
2022-02-13 10:18:56 -0600

    sequilize port error

--- config/connection.js
+++ config/connection.js
@@ -3,13 +3,15 @@ const Sequelize = require('sequelize');
 require('dotenv').config();
 
 // create connection to our db
-if (process.env.JAWSDB_URL) {
-    sequelize = new Sequelize(process.env.JAWSDB_URL);
-} else {
-    sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-        host: 'localhost',
-        dialect: 'mysql',
-        port: 3306
+var database = process.env.DATABASE_URL || 'campeonatodb'
+var sequelize = ""
+
+if (process.env.DATABASE_URL) {
+    sequelize = new Sequelize(database)
+}
+else {
+    sequelize = new Sequelize(database, 'postgres', '', {
+        dialect: 'postgres'
     });
 }
 
